57fd6f9b38415e5e2e56e35c38c3d65acf0d47ac
altramax <ajayiezekiel559@gmail.com>
2024-12-09 17:05:39 +0100

    refactor

--- src/Components/ProjectGroup/ProjectGroup.tsx
+++ src/Components/ProjectGroup/ProjectGroup.tsx
@@ -1,7 +1,5 @@
 import ProjectGroupStyle from "./ProjectGroupStyle";
 import Projects from "../Projects/Projects";
-import calculator from "../../assets/images/calculator.png";
-import etravels from "../../assets/images/e-travels.png";
 import weather from "../../assets/images/weather app.png";
 import note from "../../assets/images/note-app.png";
 import ecommerce from "../../assets/images/og.png";
